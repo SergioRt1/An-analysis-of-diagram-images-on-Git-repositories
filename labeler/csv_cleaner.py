30fcb0895359fa03a3cf7cf19e0456cadc979554
Sergio Rodríguez <sergio.rodriguezt@rappi.com>
2022-09-06 03:24:18 -0500

    Add extra cases to dataset

--- labeler/csv_cleaner.py
+++ labeler/csv_cleaner.py
@@ -34,23 +34,15 @@ def clean_data(images_map):
         if category == '.':
             image = Path(dataset_folder + image_name)
             image.unlink(missing_ok=True)
-        elif category == '7':
-            category = '6'
-            categories_map[category] = categories_map[category] + 1
-            data.append([image_name, category])
         else:
             categories_map[category] = categories_map[category] + 1
             data.append([image_name, category])
-
-    print(categories_map)
+    total = 0
+    for category in sorted(categories_map.keys()):
+        print(f'{category}: {categories_map[category]}')
+        total += categories_map[category]
+    print(f'Total: {total}')
     return data
-
-
-def main():
-    images_map = csv_to_dict()
-    data = clean_data(images_map)
-
-    write_filtered_csv(data)
 
 
 def write_filtered_csv(data):
@@ -58,6 +50,13 @@ def write_filtered_csv(data):
         writer = csv.writer(f)
         writer.writerow(['Name', 'Category'])
         writer.writerows(data)
+
+
+def main():
+    images_map = csv_to_dict()
+    data = clean_data(images_map)
+
+    write_filtered_csv(data)
 
 
 main()
